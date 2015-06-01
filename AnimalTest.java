c441ff635534df4bb5ade8d8236214eb41636d7e
Evan <ultrastale@users.noreply.github.com>
2015-06-01 15:05:52 -0700

    Create AnimalTest.java

---
+++ AnimalTest.java
@@ -0,0 +1,21 @@
+import acm.program.GraphicsProgram;
+
+
+public class AnimalTest extends GraphicsProgram{
+	
+	public void run() {
+		
+		Animal buffalo = new Animal("Buffalo", "plains", "tyson.gif", 100);
+		
+		System.out.println("Name: " + buffalo.getName());
+		System.out.println("Habitat: " + buffalo.getHabitat());
+		System.out.println("Name: " + buffalo.getValue());
+		
+		add(buffalo.getImage());
+		Animal.foo(); // static method call
+		
+		
+		
+	}
+
+}
